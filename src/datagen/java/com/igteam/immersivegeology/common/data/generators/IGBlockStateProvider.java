2e72af5fe5471e8349dc0c10ed204e65ee0a8bff
MrMagnusneo <103108262+MrMagnusneo@users.noreply.github.com>
2026-08-24 20:49:59 +0300

    fix: load multiblock templates from 1.21 path

--- src/datagen/java/com/igteam/immersivegeology/common/data/generators/IGBlockStateProvider.java
+++ src/datagen/java/com/igteam/immersivegeology/common/data/generators/IGBlockStateProvider.java
@@ -955,7 +955,7 @@ public class IGBlockStateProvider extends BlockStateProvider {
         final ResourceLocation name = multiblock.getUniqueName();
         if(TemplateMultiblock.SYNCED_CLIENT_TEMPLATES.containsKey(name))
             return;
-        final String filePath = "structures/"+name.getPath()+".nbt";
+        final String filePath = "structure/"+name.getPath()+".nbt";
         int slash = filePath.indexOf('/');
         String prefix = filePath.substring(0, slash);
         ResourceLocation shortLoc = ResourceLocation.fromNamespaceAndPath(
